c0b16c5756e5f2ab496a55da2c7efa77acbd7dca
新井 恒陽 <koyo.arai@i-studio.co.jp>
2020-03-25 13:44:35 +0900

    added

--- mobile/NotchDevice/DeviceInfoModel.cs
+++ mobile/NotchDevice/DeviceInfoModel.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 public class DeviceInfoModel : MonoBehaviour
 {
+
     /// <summary>
     /// ノッチがあるデバイスかどうか判定
     /// </summary>
     /// <returns>ノッチがあるデバイスかどうか</returns>
     public static bool hasNotchDevice() 
     {
+#if UNITY_EDITOR
+        bool isEditorDeviceHasNotchDecision = false;
+        return isEditorDeviceHasNotchDecision;
+#else
         return !(Screen.height == Screen.safeArea.height);
+#endif
     }
 }
+
 
